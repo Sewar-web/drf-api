49e1f85761583a8991d37f6e12024bb37ca7dc34
Sewar-web <s.maqableh@yahoo.com>
2021-07-25 19:19:39 +0300

    done

---
+++ moviesapp/views.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Movies
+from .serializer import MoviesSerializer
+
+# Create your views here.
+class MoviesListView(generics.ListCreateAPIView):    
+    serializer_class = MoviesSerializer
+    queryset = Movies.objects.all()
+
+class MoviesDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MoviesSerializer
+    queryset = Movies.objects.all()
